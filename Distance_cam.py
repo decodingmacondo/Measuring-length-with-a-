c5ff483acdaf0a77fbcc686a730607efa397f930
decodingmacondo <46906371+decodingmacondo@users.noreply.github.com>
2019-02-01 14:18:48 -0500

    Update Distance_cam.py

--- Distance_cam.py
+++ Distance_cam.py
@@ -7,8 +7,8 @@ point1 = ()
 point2 = ()
 d=[]
  
-def midpoint(ptA, ptB):
-    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5) 
+def midpoint(point1, point2):
+    return ((point1[0] + point2[0]) * 0.5, (point1[1] + point2[1]) * 0.5) 
 
 def mouse_drawing(event, x, y, flags, params):
     global point1, point2, drawing
@@ -39,8 +39,8 @@ while True:
     if point1 and point2:
         cv2.line(frame, point1, point2, (0, 255, 0))
         dA = dist.euclidean((int(point1[0]), int(point1[1])), (int(point2[0]), int(point2[1])))
-        (tltrX, tltrY) = midpoint(point1, point2)
-        cv2.putText(frame, "{:.1f}px".format(dA),(int(tltrX - 25), int(tltrY - 20)), cv2.FONT_HERSHEY_SIMPLEX,0.65, (255, 255, 255), 1)
+        (tX, tY) = midpoint(point1, point2)
+        cv2.putText(frame, "{:.1f}px".format(dA),(int(tX - 25), int(tY - 20)), cv2.FONT_HERSHEY_SIMPLEX,0.65, (255, 255, 255), 1)
  
     cv2.imshow("Frame", frame)
  
